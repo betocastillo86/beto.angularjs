2f170320503e2f3ce32608c125f62211dcb5fb9a
Gabriel Castillo <gabriel.castillo86@hotmail.com>
2018-04-06 12:58:10 -0500

    Ajuste a google analytics

--- src/services/common/analytics.service.js
+++ src/services/common/analytics.service.js
@@ -21,11 +21,17 @@
             if (app.Settings.general.googleAnalyticsCode.length) {
                 thewindow.ga('send', 'pageview', thelocation.path());
             }
+            else{
+                console.log('Visita rastreada');
+            }
         }
 
-        function trackGoal(category, action, label, value) {
+        function trackGoal(thewindow, category, action, label, value) {
             if (app.Settings.general.googleAnalyticsCode.length) {
-                $window.ga('send', 'event', category, action, label, value);
+                thewindow.ga('send', 'event', category, action, label, value);
+            }
+            else{
+                console.log('Goal rastreado', category, action, label, value);
             }
         }
     }
